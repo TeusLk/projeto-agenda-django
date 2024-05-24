8084265a77d1269395ca7b06c71d567a77dc640a
Mateus Santos <mateussantos200213@gmail.com>
2024-05-24 15:24:21 -0300

    campo owner

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -24,6 +25,10 @@ class Contact(models.Model):
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/') # Pra onde vai a imagem enviada... E se é obrigatorio ou não.
 
     category = models.ForeignKey(Category, 
+                                 on_delete=models.SET_NULL,
+                                 blank=True, null=True)
+    
+    owner = models.ForeignKey(User, 
                                  on_delete=models.SET_NULL,
                                  blank=True, null=True)
 
