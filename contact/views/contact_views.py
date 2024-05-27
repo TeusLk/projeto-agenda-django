750ba7f3adbc603a72e415ef9829afafc2e89b54
Mateus Santos <mateussantos200213@gmail.com>
2024-05-27 17:32:18 -0300

    edit painel adm

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -3,8 +3,8 @@ from contact.models import Contact
 
 def index(request):
 
-    contacts = Contact.objects.all()
-
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+    
     context = {
         'contacts': contacts,
     }
