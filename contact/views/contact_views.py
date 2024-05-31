c9a011da50745042d5ae14d15c5cb86867fd608b
Mateus Santos <mateussantos200213@gmail.com>
2024-05-31 17:31:41 -0300

    parciais

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -4,9 +4,10 @@ from contact.models import Contact
 def index(request):
 
     contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
-    
+
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - '
     }
 
     return render(
@@ -22,8 +23,11 @@ def contact(request, contact_id):
     single_contact = get_object_or_404(
         Contact, pk=contact_id, show=True)
     
+    contact_name = f'{single_contact.first_name} {single_contact.last_name} -'
+
     context = {
         'contact': single_contact,
+        'site_title': contact_name
     }
 
     return render(
